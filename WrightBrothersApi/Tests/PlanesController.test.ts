f7a0209e49d225e00c5c5d815ecbee22e31495ea
oxym <32470787+oxym@users.noreply.github.com>
2024-06-11 00:45:05 -0600

    refactor: Update PlanesController import and router initialization

--- WrightBrothersApi/Tests/PlanesController.test.ts
+++ WrightBrothersApi/Tests/PlanesController.test.ts
@@ -1,26 +1,27 @@
 import request from 'supertest';
 import express, { Express } from 'express';
-import planesController from '../Controllers/planesController';
+import PlanesController from '../Controllers/PlanesController';
 
 let app: Express;
 
 beforeAll(() => {
-  app = express();
-  app.use(express.json());
-  app.use('/', planesController);
+    const planesController = new PlanesController();
+    app = express();
+    app.use(express.json());
+    app.use('/', planesController.getRouter());
 });
 
-describe('planesController', () => {
-  test('GET / - returns list of planes', async () => {
-    const res = await request(app).get('/');
+describe('PlanesController', () => {
+    test('GET / - returns list of planes', async () => {
+        const res = await request(app).get('/');
 
-    expect(res.statusCode).toEqual(200);
-    // the body should have non-zero length
-    expect(res.body).not.toHaveLength(0);
-    expect(res.body[0]).toHaveProperty('id');
-    expect(res.body[0]).toHaveProperty('name');
-    expect(res.body[0]).toHaveProperty('year');
-    expect(res.body[0]).toHaveProperty('description');
-    expect(res.body[0]).toHaveProperty('rangeInKm');
-  });
+        expect(res.statusCode).toEqual(200);
+        // the body should have non-zero length
+        expect(res.body).not.toHaveLength(0);
+        expect(res.body[0]).toHaveProperty('id');
+        expect(res.body[0]).toHaveProperty('name');
+        expect(res.body[0]).toHaveProperty('year');
+        expect(res.body[0]).toHaveProperty('description');
+        expect(res.body[0]).toHaveProperty('rangeInKm');
+    });
 });
