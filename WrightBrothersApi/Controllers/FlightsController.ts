717035f13c31d05f854b6d78cab2c5b25c4b9a87
oxym <32470787+oxym@users.noreply.github.com>
2024-09-04 13:11:16 -0600

    using inefficient algorithm for prime checking

--- WrightBrothersApi/Controllers/FlightsController.ts
+++ WrightBrothersApi/Controllers/FlightsController.ts
@@ -264,9 +264,7 @@ class flightsController {
 
     private isPrime(num: number): boolean {
         if (num <= 1) return false;
-        if (num === 2) return true;
-        if (num % 2 === 0) return false;
-        for (let i = 3, sqrt = Math.sqrt(num); i <= sqrt; i += 2) {
+        for (let i = 2; i < num; i++) {
             if (num % i === 0) return false;
         }
         return true;
